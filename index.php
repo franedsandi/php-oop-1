f50c8c3671d65e50aca2abd56ff551c653065db8
franedsandi <franedsandi@gmail.com>
2023-11-06 17:50:02 +0100

    [feat] movies in a separate file

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 require_once __DIR__ . '/Model/movie.php';
 require_once __DIR__ . '/Model/genre.php';
-
-$movies = [
-  new Movie('Green Lantern', 'DC', new Genre('Action', 'Romance'), 20),
-  new Movie('Avengers', 'Marvel', new Genre('Action', 'Romance'), 100),
-  new Movie('Black Widow', 'Marvel', new Genre('Action', 'Romance'), 70),
-  new Movie('Antman Quantummania', 'Marvel', new Genre('Action', 'Romance'), 40),
-  new Movie('Aquaman', 'DC', new Genre('Action', 'Romance'), 600)
-];
-
-var_dump($movies);
+require_once __DIR__ . './db.php'
 ?>
 
 <!DOCTYPE html>
