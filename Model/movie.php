6668b59f5269f09af4ecaffea89f290127e51c65
franedsandi <franedsandi@gmail.com>
2023-11-06 17:04:46 +0100

    [feat] require

---
+++ Model/movie.php
@@ -0,0 +1,25 @@
+<?php
+class Movie {
+
+    public $name;
+    public $productors_company;
+    public $genre;
+    public $isGood;
+
+    public function setIsGood($punteggio) {
+      if ($punteggio > 50) {
+        $this->isGood = 'is a good movie';
+      } else {
+        $this->isGood = 'is a terrible movie';
+      }
+    }
+    public function getFullName(){
+      return "{$this->productors_company} {$this->name}";
+    }
+
+    public function __construct($_name, $_productors_company){
+      $this->name = $_name;
+      $this->productors_company = $_productors_company;
+    }
+  }
+  ?>
