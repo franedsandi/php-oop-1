42969207b180754e0bed5f154817c8fdccd0a0ab
franedsandi <franedsandi@gmail.com>
2023-11-07 10:09:30 +0100

    organize

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$movies = [
-  new Movie('Green Lantern', 'DC', new Genre('Action', 'Romance'), 20),
-  new Movie('Avengers', 'Marvel', new Genre('Action', 'Romance'), 100),
-  new Movie('Black Widow', 'Marvel', new Genre('Action', 'Romance'), 70),
-  new Movie('Antman Quantummania', 'Marvel', new Genre('Action', 'Romance'), 40),
-  new Movie('Aquaman', 'DC', new Genre('Action', 'Romance'), 600)
-];
+  $movies = [
+    new Movie('Green Lantern', 'DC', new Genre('Action', 'Romance'), 20),
+    new Movie('Avengers', 'Marvel', new Genre('Action', 'Romance'), 100),
+    new Movie('Black Widow', 'Marvel', new Genre('Action', 'Romance'), 70),
+    new Movie('Antman Quantummania', 'Marvel', new Genre('Action', 'Romance'), 40),
+    new Movie('Aquaman', 'DC', new Genre('Action', 'Romance'), 600)
+  ];
 ?>
