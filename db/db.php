c5fc1c415b2517d101952c22b4887dfa66791dd4
franedsandi <franedsandi@gmail.com>
2023-11-08 17:37:40 +0100

    [fix] forgot to add the genre back to the tv serie

--- db/db.php
+++ db/db.php
@@ -3,7 +3,7 @@
     new movie('Green Lantern', 'DC', ['Action', 'Animation'], 20, 2011, "100 mins"),
     new tvserie('Avatar: The legend of Angg', 'Avatar Studios', ['Action', 'Animation'], 100, 2005, 2008, 61, 3),
     new movie('Avengers', 'Marvel', ['Action', 'Drama'], 100, 2012, "143 mins" ),
-    new tvserie('Loki', 'Marvel', [], 70, 2021, 2023, 16, 2),
+    new tvserie('Loki', 'Marvel', ['Action', 'Animation'], 70, 2021, 2023, 16, 2),
     new movie('Black Widow', 'Marvel', ['Animation', 'Action'], 70, 2021, "133 mins"),
     new tvserie('Hajime no Ippo', 'Mad House', ['Sport', 'Anime'], 1000, 2000, 2002, 127, 3),
     new movie('Antman Quantummania', 'Marvel', ['Animation', 'Drama'], 40, 2023, "125 mins"),
