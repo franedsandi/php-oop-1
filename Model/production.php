63f1b712d4629598d08b833a8d09ba628cadbb42
franedsandi <franedsandi@gmail.com>
2023-11-08 17:29:46 +0100

    [feat] more errors, just in case

--- Model/production.php
+++ Model/production.php
@@ -21,13 +21,29 @@ class production {
   }
   
   public function __construct(string $_name, string $_productors_company, array $_genre , float $_punteggio, float $_year = null){
-    $this->name = $_name;
-    $this->productors_company = $_productors_company;
-    if(empty($_genre)){
-      throw new Exception("there is no movie or tvserie witout any genre");
+    if(empty($_name)){
+      throw new Exception("dont forget that everything have a name ... ");
     }else{
-      $this->genre = $_genre;
+      $this->name = $_name;
     }
+    if(empty($_productors_company)){
+      throw new Exception("dude how dont you know who made $this->name ... ");
+    }else{
+      $this->productors_company = $_productors_company;
+    }
+    
+    if(empty($_genre)){
+      $classType = get_class($this);
+      if($classType === "movie"){
+          throw new Exception("There is no movie without any genre, i'm talking about $this->name the $this->productors_company movie" );
+      } elseif($classType === "tvserie"){
+          throw new Exception("There is no TV series without any genre, i'm talking about $this->name the $this->productors_company TV serie");
+      } else {
+          throw new Exception("Unknown production type");
+      }
+  } else {
+      $this->genre = $_genre;
+  }
     $this->setIsGood($_punteggio);
     $this->year = $_year;
   }
